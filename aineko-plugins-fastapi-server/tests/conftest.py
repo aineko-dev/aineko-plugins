3f12eec7f215b8626b0fc13681dee1e19f07baa5
Barabazs <31799121+Barabazs@users.noreply.github.com>
2024-02-01 14:40:17 +0100

    chore: remove obsolete fixtures

--- aineko-plugins-fastapi-server/tests/conftest.py
+++ aineko-plugins-fastapi-server/tests/conftest.py
@@ -1,44 +1,9 @@
 # Copyright 2023 Aineko Authors
 # SPDX-License-Identifier: Apache-2.0
 """Fixtures for aineko-plugins"""
-import os
-
 import pytest
-from aineko import ConfigLoader, Runner
 from aineko.__main__ import cli
 from click.testing import CliRunner
-
-
-@pytest.fixture(scope="module")
-def conf_directory():
-    """Config directory fixture.
-
-    Returns:
-        str: Path to config directory
-    """
-    return os.path.join(os.path.dirname(__file__), "conf")
-
-
-@pytest.fixture(scope="module")
-def config_loader(test_pipeline_config_file: str):
-    """Config loader fixture.
-
-    Returns:
-        ConfigLoader: Test config loader
-    """
-    return ConfigLoader(
-        pipeline_config_file=test_pipeline_config_file,
-    )
-
-
-@pytest.fixture(scope="module")
-def runner(test_pipeline_config_file: str):
-    """Runner fixture.
-
-    Returns:
-        Runner: Test runner
-    """
-    return Runner(pipeline_config_file=test_pipeline_config_file)
 
 
 @pytest.fixture(scope="function")
