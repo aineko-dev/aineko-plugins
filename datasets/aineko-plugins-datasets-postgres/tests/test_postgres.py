ff4eb3c1671c575d943c7b9208f188ac9ef22765
Barabazs <31799121+Barabazs@users.noreply.github.com>
2024-02-18 17:38:02 +0100

    tests: add more test cases

--- datasets/aineko-plugins-datasets-postgres/tests/test_postgres.py
+++ datasets/aineko-plugins-datasets-postgres/tests/test_postgres.py
@@ -26,12 +26,33 @@ async def test_exists(database_with_table, postgres_db: AsyncPostgresDataset):
 
 
 @pytest.mark.asyncio
-async def test_delete(database_with_table, postgres_db: AsyncPostgresDataset):
+async def test_delete(
+    database_with_table,
+    postgres_db: AsyncPostgresDataset,
+    subtests,
+):
     """Test the delete method."""
-    async with postgres_db as dataset:
-        assert await dataset.exists() is True
-        await dataset.delete(if_exists=True)
-        assert await dataset.exists() is False
+    with subtests.test("Test delete without if_exists"):
+        async with postgres_db as dataset:
+            assert await dataset.exists() is True
+            await dataset.delete()
+            assert await dataset.exists() is False
+
+    with subtests.test("Test delete with if_exists(True)"):
+        async with postgres_db as dataset:
+            assert await dataset.exists() is False
+            await dataset.create(
+                schema={"id": "SERIAL PRIMARY KEY", "name": "VARCHAR(255)"},
+            )
+            assert await dataset.exists() is True
+            await dataset.delete(if_exists=True)
+            assert await dataset.exists() is False
+
+    with subtests.test("Test delete with if_exists(False)"):
+        async with postgres_db as dataset:
+            assert await dataset.exists() is False
+            with pytest.raises(Exception):
+                await dataset.delete(if_exists=False)
 
 
 @pytest.mark.asyncio
@@ -44,3 +65,28 @@ async def test_create(
             schema={"id": "SERIAL PRIMARY KEY", "name": "VARCHAR(255)"},
         )
         assert await postgres_db.exists()
+
+
+@pytest.mark.asyncio
+async def test_write(database_with_table, postgres_db: AsyncPostgresDataset):
+    """Test the write method."""
+    async with postgres_db as dataset:
+        await dataset.write(
+            query="INSERT INTO test_table (name) VALUES ('test')",
+        )
+        cur = database_with_table.cursor()
+        cur.execute("SELECT * FROM test_table")
+        result = cur.fetchall()
+        assert len(result) == 4
+        cur.close()
+
+
+@pytest.mark.asyncio
+async def test_read(database_with_table, postgres_db: AsyncPostgresDataset):
+    """Test the read method."""
+    async with postgres_db as dataset:
+        data = await dataset.read(query="SELECT * FROM test_table")
+        expected_data = ["foo", "bar", "baz"]
+        assert len(data) == 3
+        for row, expected in zip(data, expected_data):
+            assert row[1] == expected
