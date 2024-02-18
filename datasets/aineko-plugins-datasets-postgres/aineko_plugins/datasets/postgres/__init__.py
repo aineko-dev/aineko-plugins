0587393232cac8197f2c787e266d09ef43564bec
Barabazs <31799121+Barabazs@users.noreply.github.com>
2024-02-18 16:09:01 +0100

    refactor: add top level import

--- datasets/aineko-plugins-datasets-postgres/aineko_plugins/datasets/postgres/__init__.py
+++ datasets/aineko-plugins-datasets-postgres/aineko_plugins/datasets/postgres/__init__.py
@@ -1,2 +1,5 @@
 # Copyright 2023 Aineko Authors
 # SPDX-License-Identifier: Apache-2.0
+from .postgres import AsyncPostgresDataset, AWSDatasetHelper
+
+__all__ = ["AsyncPostgresDataset", "AWSDatasetHelper"]
